537e5f25a607b52771797c60626b4f7f49680400
alitsynka <alina.strok@icloud.com>
2025-02-13 18:32:07 +0300

    sec commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export", // Позволяет экспортировать статические файлы
+    basePath: "/lootster", // Укажите имя репозитория
+    assetPrefix: "/lootster/",
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
