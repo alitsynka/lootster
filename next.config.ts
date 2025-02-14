4985dd474d7f38aa03f75c845f43557e962e6a7b
alitsynka <alina.strok@icloud.com>
2025-02-14 11:33:14 +0300

    added chanhes to package-json file

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
     output: "export", // Позволяет экспортировать статические файлы
     basePath: "/lootster", // Укажите имя репозитория
