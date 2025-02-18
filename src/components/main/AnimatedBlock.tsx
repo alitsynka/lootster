507be24f2568f5f2f9285b0b9da3c92e1dda8c49
alitsynka <alina.strok@icloud.com>
2025-02-18 11:08:05 +0300

    fix mobile

--- src/components/main/AnimatedBlock.tsx
+++ src/components/main/AnimatedBlock.tsx
@@ -1,5 +1,6 @@
 'use client'
 import {motion, useInView} from "framer-motion";
+import style from './Main.module.scss';
 import Image from "next/image";
 import portal from "@/assets/images/portal.png";
 import {useRef} from "react";
@@ -7,20 +8,21 @@ import {useRef} from "react";
 export const AnimatedBlock = () => {
 
     const ref = useRef(null)
-    const isInView = useInView(ref, { once: true, amount: 0.3 });
+    const isInView = useInView(ref, { once: false, amount: 0.5 });
+
     return (
         <motion.div
             ref={ref}
             initial={{y: 0}}
-            animate={isInView ? { y: [0, -30, 0] } : ''} // Slightly smaller bounce for a smoother effect
+            animate={isInView ? { y: [0, -30, 0] } : {y: 0}} // Slightly smaller bounce for a smoother effect
             transition={{
                 duration: 2.5, // Increase duration for a slower, smoother movement
-                stiffness: 800, // Lower stiffness for a smoother bounce
-                damping: 500, // Higher damping for a more controlled, subtle motion
-                repeat: Infinity,
+                repeat: isInView ? Infinity : 0,
                 repeatType: "reverse",
                 ease: "easeOut", // 'easeOut' will make it slow down at the end for smoothness
-            }}>
+            }}
+            className={style.SectionImg}
+        >
             <Image src={portal} alt={'portal'}/>
         </motion.div>
     )
