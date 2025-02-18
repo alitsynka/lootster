89976c5c7ebefd99a66f11e0cbdc55c115ea7a8f
alitsynka <alina.strok@icloud.com>
2025-02-18 14:53:07 +0300

    added vikas fixes 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type {Metadata} from "next";
 import {Press_Start_2P} from "next/font/google";
 import "../assets/styles/reset.scss";
 import Head from "next/head";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
